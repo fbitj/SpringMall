f4a107f035d34a7e779c0110e64b6229f81fa780
yemonfish <s_unflower@163.com>
2019-11-17 00:40:57 +0800

    2019-11-16 单表版本

--- src/main/java/com/springmall/controller/ConfigController.java
+++ src/main/java/com/springmall/controller/ConfigController.java
@@ -2,15 +2,17 @@ package com.springmall.controller;
 
 import com.springmall.bean.*;
 
-import com.springmall.service.ExpressConfigService;
-import com.springmall.service.MallConfigService;
-import com.springmall.service.OrderConfigService;
-import com.springmall.service.WxConfigService;
+import com.springmall.service.SystemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 处理后台系统所有关于config模块下的请求
@@ -24,13 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConfigController {
 
     @Autowired
-    MallConfigService mallConfigService;
-    @Autowired
-    ExpressConfigService expressConfigService;
-    @Autowired
-    OrderConfigService orderConfigService;
-    @Autowired
-    WxConfigService wxConfigService;
+    SystemService systemService;
 
     /**
      * 获取商场配置信息
@@ -49,7 +45,7 @@ public class ConfigController {
     @RequestMapping(value = "mall", method = RequestMethod.GET)
     public BaseReqVo queryMallConfig(){
         BaseReqVo baseReqVo = new BaseReqVo();
-        MallConfig queryMallConfig = mallConfigService.queryMallConfig();
+        MallConfig queryMallConfig = systemService.queryMallConfig();
         baseReqVo.setErrno(0);
         baseReqVo.setData(queryMallConfig);
         baseReqVo.setErrmsg("获取商场配置信息成功！");
@@ -58,13 +54,10 @@ public class ConfigController {
 
     /**
      * 更新商场配置信息
-     * @param mallConfig 获取参数：
-     * {
-     * 	"litemall_mall_phone": "10086",
-     * 	"litemall_mall_address": "武汉",
-     * 	"litemall_mall_name": "光谷店",
-     * 	"litemall_mall_qq": "123456789"
-     * }
+     * @param litemall_mall_phone 商场联系电话
+     * @param litemall_mall_address 商场地址
+     * @param litemall_mall_name 商场名称
+     * @param litemall_mall_qq 商场联系QQ
      * @return 返回参数：
      * {
      * 	"errno": 0,
@@ -72,9 +65,31 @@ public class ConfigController {
      * }
      */
     @RequestMapping(value = "mall", method = RequestMethod.POST)
-    public BaseReqVo updateMallConfig(@RequestBody MallConfig mallConfig){
+    public BaseReqVo updateMallConfig(
+            @RequestBody String litemall_mall_phone,
+            @RequestBody String litemall_mall_address,
+            @RequestBody String litemall_mall_name,
+            @RequestBody String litemall_mall_qq
+    ){
+        Map<String,String> map1 = new HashMap<>();
+        Map<String,String> map2 = new HashMap<>();
+        Map<String,String> map3 = new HashMap<>();
+        Map<String,String> map4 = new HashMap<>();
+        map1.put("paramName","cskaoyan_mall_mall_name");
+        map1.put("paramValue",litemall_mall_name);
+        map2.put("paramName","cskaoyan_mall_mall_address");
+        map2.put("paramValue",litemall_mall_address);
+        map3.put("paramName","cskaoyan_mall_mall_phone");
+        map3.put("paramValue",litemall_mall_phone);
+        map4.put("paramName","cskaoyan_mall_mall_qq");
+        map4.put("paramValue",litemall_mall_qq);
+        List<Map<String,String>> paramMapList = new ArrayList<>();
+        paramMapList.add(map1);
+        paramMapList.add(map2);
+        paramMapList.add(map3);
+        paramMapList.add(map4);
         BaseReqVo baseReqVo = new BaseReqVo();
-        int result = mallConfigService.updateMallConfig(mallConfig);
+        int result = systemService.updateMallConfig(paramMapList);
         if(result==1){
             baseReqVo.setErrno(0);
             baseReqVo.setErrmsg("修改商场配置信息成功！");
@@ -104,7 +119,7 @@ public class ConfigController {
     @RequestMapping(value = "express", method = RequestMethod.GET)
     public BaseReqVo queryExpressConfig() {
         BaseReqVo baseReqVo = new BaseReqVo();
-        ExpressConfig queryExpressConfig = expressConfigService.queryExpressConfig();
+        ExpressConfig queryExpressConfig = systemService.queryExpressConfig();
         baseReqVo.setErrno(0);
         baseReqVo.setData(queryExpressConfig);
         baseReqVo.setErrmsg("获取运费配置信息成功！");
@@ -113,13 +128,8 @@ public class ConfigController {
 
     /**
      * 更新运费配置信息
-     * @param expressConfig 获取参数：
-     * {
-     * 	"litemall_mall_phone": "10086",
-     * 	"litemall_mall_address": "武汉",
-     * 	"litemall_mall_name": "光谷店",
-     * 	"litemall_mall_qq": "123456789"
-     * }
+     * @param litemall_express_freight_min 运费满减所需最低消费
+     * @param litemall_express_freight_value 运费满减不足所需运费
      * @return 返回参数：
      * {
      * 	"errno": 0,
@@ -127,9 +137,21 @@ public class ConfigController {
      * }
      */
     @RequestMapping(value = "express", method = RequestMethod.POST)
-    public BaseReqVo updateExpressConfig(@RequestBody ExpressConfig expressConfig) {
+    public BaseReqVo updateExpressConfig(
+            @RequestBody String litemall_express_freight_min,
+            @RequestBody String litemall_express_freight_value
+    ) {
+        Map<String,String> map1 = new HashMap<>();
+        Map<String,String> map2 = new HashMap<>();
+        map1.put("paramName","cskaoyan_mall_express_freight_min");
+        map1.put("paramValue",litemall_express_freight_min);
+        map2.put("paramName","cskaoyan_mall_express_freight_value");
+        map2.put("paramValue",litemall_express_freight_value);
+        List<Map<String,String>> paramMapList = new ArrayList<>();
+        paramMapList.add(map1);
+        paramMapList.add(map2);
         BaseReqVo baseReqVo = new BaseReqVo();
-        int result = expressConfigService.updateExpressConfig(expressConfig);
+        int result = systemService.updateExpressConfig(paramMapList);
         if(result==1) {
             baseReqVo.setErrno(0);
             baseReqVo.setErrmsg("修改运费配置信息成功！");
@@ -141,15 +163,11 @@ public class ConfigController {
             return baseReqVo;
         }
         if(result==3){
-            baseReqVo.setErrmsg("运费满减所需最低消费需大于0！");
+            baseReqVo.setErrmsg("参数需大于0！");
             return baseReqVo;
         }
         if(result==4){
-            baseReqVo.setErrmsg("运费满减不足所需运费需大于0！");
-            return baseReqVo;
-        }
-        if(result==5){
-            baseReqVo.setErrmsg("参数需输入数字！");
+            baseReqVo.setErrmsg("参数需输入数值！");
             return baseReqVo;
         }
         baseReqVo.setErrmsg("修改运费配置信息失败！");
@@ -172,7 +190,7 @@ public class ConfigController {
     @RequestMapping(value = "order", method = RequestMethod.GET)
     public BaseReqVo queryOrderConfig() {
         BaseReqVo baseReqVo = new BaseReqVo();
-        OrderConfig queryOrderConfig = orderConfigService.queryOrderConfig();
+        OrderConfig queryOrderConfig = systemService.queryOrderConfig();
         baseReqVo.setErrno(0);
         baseReqVo.setData(queryOrderConfig);
         baseReqVo.setErrmsg("获取订单配置信息成功！");
@@ -181,12 +199,9 @@ public class ConfigController {
 
     /**
      * 更新订单配置信息
-     * @param orderConfig 获取参数：
-     * {
-     * 	"litemall_order_comment": "88888",
-     * 	"litemall_order_unpaid": "11",
-     * 	"litemall_order_unconfirm": "454545"
-     * }
+     * @param litemall_order_comment 确认收货后超期天数
+     * @param litemall_order_unpaid 用户下单后超时分钟数
+     * @param litemall_order_unconfirm 订单发货后超期天数
      * @return 返回参数：
      * {
      * 	"errno": 0,
@@ -194,9 +209,26 @@ public class ConfigController {
      * }
      */
     @RequestMapping(value = "order", method = RequestMethod.POST)
-    public BaseReqVo updateOrderConfig(@RequestBody OrderConfig orderConfig) {
+    public BaseReqVo updateOrderConfig(
+            @RequestBody String litemall_order_comment,
+            @RequestBody String litemall_order_unpaid,
+            @RequestBody String litemall_order_unconfirm
+    ) {
+        Map<String,String> map1 = new HashMap<>();
+        Map<String,String> map2 = new HashMap<>();
+        Map<String,String> map3 = new HashMap<>();
+        map1.put("paramName","cskaoyan_mall_order_comment");
+        map1.put("paramValue",litemall_order_comment);
+        map2.put("paramName","cskaoyan_mall_order_unpaid");
+        map2.put("paramValue",litemall_order_unpaid);
+        map3.put("paramName","cskaoyan_mall_order_unconfirm");
+        map3.put("paramValue",litemall_order_unconfirm);
+        List<Map<String,String>> paramMapList = new ArrayList<>();
+        paramMapList.add(map1);
+        paramMapList.add(map2);
+        paramMapList.add(map3);
         BaseReqVo baseReqVo = new BaseReqVo();
-        int result = orderConfigService.updateOrderConfig(orderConfig);
+        int result = systemService.updateOrderConfig(paramMapList);
         if(result==1) {
             baseReqVo.setErrno(0);
             baseReqVo.setErrmsg("修改订单配置信息成功！");
@@ -208,19 +240,11 @@ public class ConfigController {
             return baseReqVo;
         }
         if(result==3){
-            baseReqVo.setErrmsg("确认收货后超期天数需大于0！");
+            baseReqVo.setErrmsg("参数需大于0！");
             return baseReqVo;
         }
         if(result==4){
-            baseReqVo.setErrmsg("用户下单后超时分钟数需大于0！");
-            return baseReqVo;
-        }
-        if(result==5){
-            baseReqVo.setErrmsg("订单发货后超期天数需大于0！");
-            return baseReqVo;
-        }
-        if(result==6){
-            baseReqVo.setErrmsg("参数需输入整数！");
+            baseReqVo.setErrmsg("参数需输入正整数！");
             return baseReqVo;
         }
         baseReqVo.setErrmsg("修改订单配置信息失败！");
@@ -247,7 +271,7 @@ public class ConfigController {
     @RequestMapping(value = "wx", method = RequestMethod.GET)
     public BaseReqVo queryWxConfig() {
         BaseReqVo baseReqVo = new BaseReqVo();
-        WxConfig queryWxConfig = wxConfigService.queryWxConfig();
+        WxConfig queryWxConfig = systemService.queryWxConfig();
         baseReqVo.setErrno(0);
         baseReqVo.setData(queryWxConfig);
         baseReqVo.setErrmsg("获取微信小程序配置信息成功！");
@@ -256,16 +280,13 @@ public class ConfigController {
 
     /**
      * 更新微信小程序配置信息
-     * @param wxConfig 获取参数：
-     * {
-     * 	"litemall_wx_index_new": "10",
-     * 	"litemall_wx_catlog_goods": "787899",
-     * 	"litemall_wx_catlog_list": "6",
-     * 	"litemall_wx_share": "false",
-     * 	"litemall_wx_index_brand": "4",
-     * 	"litemall_wx_index_hot": "51",
-     * 	"litemall_wx_index_topic": "6"
-     * }
+     * @param litemall_wx_index_new 新品首发栏目商品显示数量
+     * @param litemall_wx_catlog_goods 分类栏目商品显示数量
+     * @param litemall_wx_catlog_list 分类栏目显示数量
+     * @param litemall_wx_share 商品分享功能开关
+     * @param litemall_wx_index_brand 品牌制造商直供栏目品牌商显示数量
+     * @param litemall_wx_index_hot 人气推荐栏目商品显示数量
+     * @param litemall_wx_index_topic 专题精选栏目显示数量
      * @return 返回参数：
      * {
      * 	"errno": 0,
@@ -273,9 +294,46 @@ public class ConfigController {
      * }
      */
     @RequestMapping(value = "wx", method = RequestMethod.POST)
-    public BaseReqVo updateWxConfig(@RequestBody WxConfig wxConfig) {
+    public BaseReqVo updateWxConfig(
+            @RequestBody String litemall_wx_index_new,
+            @RequestBody String litemall_wx_catlog_goods,
+            @RequestBody String litemall_wx_catlog_list,
+            @RequestBody String litemall_wx_share,
+            @RequestBody String litemall_wx_index_brand,
+            @RequestBody String litemall_wx_index_hot,
+            @RequestBody String litemall_wx_index_topic
+    ) {
+        Map<String,String> map1 = new HashMap<>();
+        Map<String,String> map2 = new HashMap<>();
+        Map<String,String> map3 = new HashMap<>();
+        Map<String,String> map4 = new HashMap<>();
+        Map<String,String> map5 = new HashMap<>();
+        Map<String,String> map6 = new HashMap<>();
+        Map<String,String> map7 = new HashMap<>();
+        map1.put("paramName","cskaoyan_mall_wx_index_new");
+        map1.put("paramValue",litemall_wx_index_new);
+        map2.put("paramName","cskaoyan_mall_wx_catlog_goods");
+        map2.put("paramValue",litemall_wx_catlog_goods);
+        map3.put("paramName","cskaoyan_mall_wx_catlog_list");
+        map3.put("paramValue",litemall_wx_catlog_list);
+        map4.put("paramName","cskaoyan_mall_wx_share");
+        map4.put("paramValue",litemall_wx_share);
+        map5.put("paramName","cskaoyan_mall_wx_index_brand");
+        map5.put("paramValue",litemall_wx_index_brand);
+        map6.put("paramName","cskaoyan_mall_wx_index_hot");
+        map6.put("paramValue",litemall_wx_index_hot);
+        map7.put("paramName","cskaoyan_mall_wx_index_topic");
+        map7.put("paramValue",litemall_wx_index_topic);
+        List<Map<String,String>> paramMapList = new ArrayList<>();
+        paramMapList.add(map1);
+        paramMapList.add(map2);
+        paramMapList.add(map3);
+        paramMapList.add(map4);
+        paramMapList.add(map5);
+        paramMapList.add(map6);
+        paramMapList.add(map7);
         BaseReqVo baseReqVo = new BaseReqVo();
-        int result = wxConfigService.updateWxConfig(wxConfig);
+        int result = systemService.updateWxConfig(paramMapList);
         if(result==1) {
             baseReqVo.setErrno(0);
             baseReqVo.setErrmsg("修改微信小程序配置信息成功！");
@@ -287,31 +345,11 @@ public class ConfigController {
             return baseReqVo;
         }
         if(result==3){
-            baseReqVo.setErrmsg("新品首发栏目商品显示数量不得为负数！");
+            baseReqVo.setErrmsg("参数需大于等于0！");
             return baseReqVo;
         }
         if(result==4){
-            baseReqVo.setErrmsg("分类栏目商品显示数量不得为负数！");
-            return baseReqVo;
-        }
-        if(result==5){
-            baseReqVo.setErrmsg("分类栏目显示数量不得为负数！");
-            return baseReqVo;
-        }
-        if(result==6){
-            baseReqVo.setErrmsg("品牌制造商直供栏目品牌商显示数量不得为负数！");
-            return baseReqVo;
-        }
-        if(result==7){
-            baseReqVo.setErrmsg("人气推荐栏目商品显示数量不得为负数！");
-            return baseReqVo;
-        }
-        if(result==8){
-            baseReqVo.setErrmsg("专题精选栏目显示数量不得为负数！");
-            return baseReqVo;
-        }
-        if(result==9){
-            baseReqVo.setErrmsg("参数需输入整数！");
+            baseReqVo.setErrmsg("参数需输入非负整数！");
             return baseReqVo;
         }
         baseReqVo.setErrmsg("修改微信小程序配置信息失败！");
---
+++ src/main/java/com/springmall/service/SystemServiceImpl.java
@@ -0,0 +1,309 @@
+package com.springmall.service;
+
+import com.springmall.bean.*;
+import com.springmall.bean.System;
+import com.springmall.mapper.SystemMapper;
+import com.springmall.utils.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class SystemServiceImpl implements SystemService {
+
+    @Autowired
+    SystemMapper systemMapper;
+
+    /**
+     * 获取商场配置信息
+     * @return 现商场配置信息对象
+     */
+    @Override
+    public MallConfig queryMallConfig () {
+        SystemExample systemExample = new SystemExample();
+        systemExample.createCriteria().andKeyNameLike("cskaoyan_mall_mall%");
+        List<System> systemList = systemMapper.selectByExample(systemExample);
+        MallConfig mallConfig = new MallConfig();
+        for (System system : systemList) {
+            if ("cskaoyan_mall_mall_name".equals(system.getKeyName())) {
+                mallConfig.setLitemall_mall_name(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_mall_address".equals(system.getKeyName())) {
+                mallConfig.setLitemall_mall_address(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_mall_phone".equals(system.getKeyName())) {
+                mallConfig.setLitemall_mall_phone(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_mall_qq".equals(system.getKeyName())) {
+                mallConfig.setLitemall_mall_qq(system.getKeyValue());
+            }
+        }
+        return mallConfig;
+    }
+
+    /**
+     * 更新商场配置信息
+     * @param paramMapList
+     * @return
+     * -1表示服务器繁忙，
+     * 0表示更新失败，
+     * 1表示更新成功，
+     * 2表示更新条件输入不完整。
+     */
+    @Override
+    public int updateMallConfig(List<Map<String,String>> paramMapList) {
+        for(Map<String,String> paramMap : paramMapList){
+            String paramName = paramMap.get("paramName");
+            String paramValue = paramMap.get("paramValue");
+            if(StringUtils.isEmpty(paramValue)){    //修改条件不完整
+                return 2;
+            }
+            System system = new System();    //set更新数据
+            system.setKeyName(paramName);
+            system.setKeyValue(paramValue);
+            SystemExample systemExample = new SystemExample();
+            systemExample.createCriteria().andKeyNameEqualTo(paramName);    //添加查找条件，根据表中key_name字段的值找到该行数据。
+            int result = 0;
+            if(systemMapper.countByExample(systemExample)==0){    //表中无该配置的相关数据，需插入一条配置信息。
+                result = systemMapper.insertSelective(system);
+                if (result==0){
+                    return result;    //foreach中有一者插入失败，则提前return。
+                }
+            }
+            else{    //表中有该配置的相关数据，更新该条配置信息。
+                result = systemMapper.updateByExampleSelective(system,systemExample);
+                if (result==0){
+                    return result;    //foreach中有一者更新失败，则提前return。
+                }
+            }
+        }
+        return 1;    //更新成功
+    }
+
+    /**
+     * 获取运费配置信息
+     * @return 现运费配置信息对象
+     */
+    @Override
+    public ExpressConfig queryExpressConfig() {
+        SystemExample systemExample = new SystemExample();
+        systemExample.createCriteria().andKeyNameLike("cskaoyan_mall_express%");
+        List<System> systemList = systemMapper.selectByExample(systemExample);
+        ExpressConfig expressConfig = new ExpressConfig();
+        for (System system : systemList) {
+            if ("cskaoyan_mall_express_freight_min".equals(system.getKeyName())) {
+                expressConfig.setLitemall_express_freight_min(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_express_freight_value".equals(system.getKeyName())) {
+                expressConfig.setLitemall_express_freight_value(system.getKeyValue());
+            }
+        }
+        return expressConfig;
+    }
+
+    /**
+     * 更新运费配置信息
+     * @param paramMapList
+     * @return
+     * -1表示服务器繁忙，
+     * 0表示更新失败，
+     * 1表示更新成功，
+     * 2表示更新条件输入不完整，
+     * 3表示更新参数存在不为正数的值，
+     * 4表示更新参数中存在非数字。
+     */
+    @Override
+    public int updateExpressConfig(List<Map<String,String>> paramMapList) {
+        for(Map<String,String> paramMap : paramMapList){
+            String paramName = paramMap.get("paramName");
+            String paramValue = paramMap.get("paramValue");
+            if(StringUtils.isEmpty(paramValue)){    //修改条件不完整
+                return 2;
+            }
+            try{
+                if (Double.parseDouble(paramValue)<=0){    //参数需大于0
+                    return 3;
+                }
+            }catch (Exception e){    //类型转换失败，存在非数字。
+                return 4;
+            }
+            System system = new System();    //set更新数据
+            system.setKeyName(paramName);
+            system.setKeyValue(paramValue);
+            SystemExample systemExample = new SystemExample();
+            systemExample.createCriteria().andKeyNameEqualTo(paramName);    //添加查找条件，根据表中key_name字段的值找到该行数据。
+            int result = 0;
+            if(systemMapper.countByExample(systemExample)==0){    //表中无该配置的相关数据，需插入一条配置信息。
+                result = systemMapper.insertSelective(system);
+                if (result==0){
+                    return result;    //foreach中有一者插入失败，则提前return。
+                }
+            }
+            else{    //表中有该配置的相关数据，更新该条配置信息。
+                result = systemMapper.updateByExampleSelective(system,systemExample);
+                if (result==0){
+                    return result;    //foreach中有一者更新失败，则提前return。
+                }
+            }
+        }
+        return 1;    //更新成功
+    }
+
+    /**
+     * 获取订单配置信息
+     * @return 现订单配置信息对象
+     */
+    @Override
+    public OrderConfig queryOrderConfig() {
+        SystemExample systemExample = new SystemExample();
+        systemExample.createCriteria().andKeyNameLike("cskaoyan_mall_order%");
+        List<System> systemList = systemMapper.selectByExample(systemExample);
+        OrderConfig orderConfig = new OrderConfig();
+        for (System system : systemList) {
+            if ("cskaoyan_mall_order_comment".equals(system.getKeyName())) {
+                orderConfig.setLitemall_order_comment(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_order_unpaid".equals(system.getKeyName())) {
+                orderConfig.setLitemall_order_unpaid(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_order_unconfirm".equals(system.getKeyName())) {
+                orderConfig.setLitemall_order_unconfirm(system.getKeyValue());
+            }
+        }
+        return orderConfig;
+    }
+
+    /**
+     * 更新订单配置信息
+     * @param paramMapList
+     * @return
+     * -1表示服务器繁忙，
+     * 0表示更新失败，
+     * 1表示更新成功，
+     * 2表示更新条件输入不完整，
+     * 3表示更新参数存在不为正数的值，
+     * 4表示更新参数中存在非整数。
+     */
+    @Override
+    public int updateOrderConfig(List<Map<String,String>> paramMapList) {
+        for(Map<String,String> paramMap : paramMapList){
+            String paramName = paramMap.get("paramName");
+            String paramValue = paramMap.get("paramValue");
+            if(StringUtils.isEmpty(paramValue)){    //修改条件不完整
+                return 2;
+            }
+            try{
+                if (Integer.parseInt(paramValue)<=0){    //参数需大于0
+                    return 3;
+                }
+            }catch (Exception e){    //类型转换失败，存在非整数。
+                return 4;
+            }
+            System system = new System();    //set更新数据
+            system.setKeyName(paramName);
+            system.setKeyValue(paramValue);
+            SystemExample systemExample = new SystemExample();
+            systemExample.createCriteria().andKeyNameEqualTo(paramName);    //添加查找条件，根据表中key_name字段的值找到该行数据。
+            int result = 0;
+            if(systemMapper.countByExample(systemExample)==0){    //表中无该配置的相关数据，需插入一条配置信息。
+                result = systemMapper.insertSelective(system);
+                if (result==0){
+                    return result;    //foreach中有一者插入失败，则提前return。
+                }
+            }
+            else{    //表中有该配置的相关数据，更新该条配置信息。
+                result = systemMapper.updateByExampleSelective(system,systemExample);
+                if (result==0){
+                    return result;    //foreach中有一者更新失败，则提前return。
+                }
+            }
+        }
+        return 1;    //更新成功
+    }
+
+    /**
+     * 获取微信小程序配置信息
+     * @return 现微信小程序配置信息对象
+     */
+    @Override
+    public WxConfig queryWxConfig() {
+        SystemExample systemExample = new SystemExample();
+        systemExample.createCriteria().andKeyNameLike("cskaoyan_mall_wx%");
+        List<System> systemList = systemMapper.selectByExample(systemExample);
+        WxConfig wxConfig = new WxConfig();
+        for (System system : systemList) {
+            if ("cskaoyan_mall_wx_index_new".equals(system.getKeyName())) {
+                wxConfig.setLitemall_wx_index_new(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_wx_catlog_goods".equals(system.getKeyName())) {
+                wxConfig.setLitemall_wx_catlog_goods(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_wx_catlog_list".equals(system.getKeyName())) {
+                wxConfig.setLitemall_wx_catlog_list(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_wx_share".equals(system.getKeyName())) {
+                wxConfig.setLitemall_wx_share(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_wx_index_brand".equals(system.getKeyName())) {
+                wxConfig.setLitemall_wx_index_brand(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_wx_index_hot".equals(system.getKeyName())) {
+                wxConfig.setLitemall_wx_index_hot(system.getKeyValue());
+            }
+            if ("cskaoyan_mall_wx_index_topic".equals(system.getKeyName())) {
+                wxConfig.setLitemall_wx_index_topic(system.getKeyValue());
+            }
+        }
+        return wxConfig;
+    }
+
+    /**
+     * 更新微信小程序配置信息
+     * @param paramMapList
+     * @return
+     * -1表示服务器繁忙，
+     * 0表示更新失败，
+     * 1表示更新成功，
+     * 2表示更新条件输入不完整，
+     * 3表示更新参数中除去商品分享功能开关，其余的参数存在负数值，
+     * 4表示更新参数中存在非整数。
+     */
+    @Override
+    public int updateWxConfig(List<Map<String,String>> paramMapList) {
+        for(Map<String,String> paramMap : paramMapList){
+            String paramName = paramMap.get("paramName");
+            String paramValue = paramMap.get("paramValue");
+            if(StringUtils.isEmpty(paramValue)){    //修改条件不完整
+                return 2;
+            }
+            try{
+                if (Integer.parseInt(paramValue)<0 && (!"cskaoyan_mall_wx_share".equals(paramValue))){    //除去商品分享功能开关，其余的参数需大于等于0。
+                    return 3;
+                }
+            }catch (Exception e){    //类型转换失败，存在非整数。
+                return 4;
+            }
+            System system = new System();    //set更新数据
+            system.setKeyName(paramName);
+            system.setKeyValue(paramValue);
+            SystemExample systemExample = new SystemExample();
+            systemExample.createCriteria().andKeyNameEqualTo(paramName);    //添加查找条件，根据表中key_name字段的值找到该行数据。
+            int result = 0;
+            if(systemMapper.countByExample(systemExample)==0){    //表中无该配置的相关数据，需插入一条配置信息。
+                result = systemMapper.insertSelective(system);
+                if (result==0){
+                    return result;    //foreach中有一者插入失败，则提前return。
+                }
+            }
+            else{    //表中有该配置的相关数据，更新该条配置信息。
+                result = systemMapper.updateByExampleSelective(system,systemExample);
+                if (result==0){
+                    return result;    //foreach中有一者更新失败，则提前return。
+                }
+            }
+        }
+        return 1;    //更新成功
+    }
+}

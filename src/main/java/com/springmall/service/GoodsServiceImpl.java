a76c62c260452d528431a6a49ad824394a36c565
liaohao <1094370130@qq.com>
2019-11-18 22:19:50 +0800

    liaohao

--- src/main/java/com/springmall/service/GoodsServiceImpl.java
+++ src/main/java/com/springmall/service/GoodsServiceImpl.java
@@ -22,14 +22,46 @@ public class GoodsServiceImpl implements GoodsService {
     Goods_specificationMapper goodsSpecificationMapper;
     @Autowired
     Goods_productMapper productMapper;
+    @Autowired
+    CategoryMapper categoryMapper;
 
+    /**
+     * 更新商品信息
+     * @param goods
+     * @param specifications
+     * @param products
+     * @param attributes
+     */
     @Override
     @Transactional
     public void updateGoodsInfo(Goods goods, List<Goods_specification> specifications, List<Goods_product> products, List<Goods_attribute> attributes) {
-        goodsMapper.updateByPrimaryKeySelective(goods);/*
-        productMapper.updateProductsByPrimaryKeySelective(products);
+        // 删除指定id的goods相关信息，然后重新写入：即更新
+        goodsMapper.updateByPrimaryKeySelective(goods);
+        //1.删除
+        Integer id = goods.getId();
+        Goods_specificationExample specificationExample = new Goods_specificationExample();
+        specificationExample.createCriteria().andGoodsIdEqualTo(id);
+        goodsSpecificationMapper.deleteByExample(specificationExample);
+        goodsSpecificationMapper.insertSpecificationsSelective(specifications);
+
+        Goods_productExample productExample = new Goods_productExample();
+        productExample.createCriteria().andGoodsIdEqualTo(id);
+        productMapper.deleteByExample(productExample);
+        for (Goods_product product : products) {
+            product.setGoodsId(goods.getId());
+        }
+        productMapper.insertProductsSelective(products);
+
+        AttributeExample attributeExample = new AttributeExample();
+        attributeExample.createCriteria().andGoodsIdEqualTo(id);
+        attributeMapper.deleteByExample(attributeExample);
+        attributeMapper.insertAttributesSelective(attributes);
+
+
+        /*goodsMapper.updateByPrimaryKeySelective(goods);
+        *//*productMapper.updateProductsByPrimaryKeySelective(products);
         attributeMapper.updateAttributesByPrimaryKeySelective(attributes);
-        goodsSpecificationMapper.updateSpecificationsByPrimaryKeySelective(specifications);*/
+        goodsSpecificationMapper.updateSpecificationsByPrimaryKeySelective(specifications);*//*
         for (Goods_specification specification : specifications) {
             goodsSpecificationMapper.updateByPrimaryKey(specification);
         }
@@ -38,9 +70,17 @@ public class GoodsServiceImpl implements GoodsService {
         }
         for (Goods_attribute attribute : attributes) {
             goodsAttributeMapper.updateByPrimaryKey(attribute);
-        }
+        }*/
     }
 
+    /**
+     *
+     * @param goods 商品信息
+     * @param specifications 商品规格
+     * @param products 商品表
+     * @param attributes 商品参数
+     * @return
+     */
     @Override
     @Transactional
     public int createGoods(Goods goods, List<Goods_specification> specifications, List<Goods_product> products, List<Goods_attribute> attributes) {
@@ -62,6 +102,11 @@ public class GoodsServiceImpl implements GoodsService {
         return 1;
     }
 
+    /**
+     * 查询商品信息
+     * @param id 商品id
+     * @return
+     */
     @Override
     public GoodInfoData queryGoodsDetailByGoodsId(int id) {
         GoodInfoData goodInfoData = new GoodInfoData();
@@ -80,7 +125,9 @@ public class GoodsServiceImpl implements GoodsService {
         productExample.createCriteria().andGoodsIdEqualTo(id);
         List<Goods_product> products = productMapper.selectByExample(productExample);
         // 封装数据
-        Integer[] categoryIds = new Integer[]{goods.getId()};
+        Integer categoryId = goods.getCategoryId();
+        Category category = categoryMapper.selectByPrimaryKey(categoryId);
+        Integer[] categoryIds = new Integer[]{category.getPid(),category.getId()};
         goodInfoData.setCategoryIds(categoryIds);
         goodInfoData.setGoods(goods);
         goodInfoData.setAttributes(attributes);
@@ -89,12 +136,27 @@ public class GoodsServiceImpl implements GoodsService {
         return goodInfoData;
     }
 
+    /**
+     * 逻辑删除商品
+     * @param goods
+     * @return
+     */
     @Override
     public int deleteGoods(Goods goods) {
         goods.setDeleted(true);
         return goodsMapper.updateByPrimaryKeySelective(goods);
     }
 
+    /**
+     * 分页查询商品
+     * @param page 当前页
+     * @param limit 每页显示数量
+     * @param goodsSn 商品编号
+     * @param name 商品名
+     * @param sortField 排序字段
+     * @param order 排序规则
+     * @return
+     */
     @Override
     public List<Goods> queryGoodsByPage(String page, String limit, String goodsSn, String name, String sortField, String order) {
         PageHelper.startPage(Integer.parseInt(page), Integer.parseInt(limit));

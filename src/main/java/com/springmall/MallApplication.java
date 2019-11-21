0c5e59ef98e23a659ed93bedc7ada799ae9ba06c
xlerong <karst>
2019-11-21 10:37:44 +0800

    定时器

--- src/main/java/com/springmall/MallApplication.java
+++ src/main/java/com/springmall/MallApplication.java
@@ -3,9 +3,11 @@ package com.springmall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.springmall.mapper")
+@EnableScheduling
 public class MallApplication {
 
     public static void main(String[] args) {
